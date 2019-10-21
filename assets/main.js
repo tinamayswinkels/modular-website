c97af2ea643d0c85cc2cd7e382b6de8cec716ecd
dezoul <aussieme@gmail.com>
2019-10-21 13:20:22 +0800

    extra layers

--- assets/main.js
+++ assets/main.js
@@ -30,21 +30,18 @@ $(document).ready(function() {
 		}
 	});
 
-
-});
-
-
-// this is the accordian code
-
-$(document).ready(function() {
-
 	var allPanels = $('.accordion > dd').hide();
 
-	$('.accordion > dt > a').click(function() {
-		allPanels.slideUp();
-		$(this).parent().next().slideDown();
-		return false;
-	});
+		$('.accordion > dt > a').click(function() {
+			allPanels.slideUp();
+
+	    $(this).filter('.open').removeClass('open').addClass('closing')
+	    $('.accordion > dt > a').removeClass('open');
+	  $(this).not(".closing").addClass('open').parent().next().slideDown();
+	    $('.accordion > dt > a').removeClass('closing');
+
+	    return false;
+		});
 
 
 });
